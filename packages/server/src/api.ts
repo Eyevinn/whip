a3e44583c4c60fa51568194f6e1eacaf6ce31c11
birme <jonas.birme@eyevinn.se>
2022-03-09 20:17:27 +0100

    WIP: correct content-type parser

--- packages/server/src/api.ts
+++ packages/server/src/api.ts
@@ -3,26 +3,26 @@ import { WHIPResource } from "./models/WHIPResource";
 import fetch from "node-fetch";
 
 export default function(fastify: FastifyInstance, opts, done) {
-  fastify.addContentTypeParser('application/sdp', async (request, payload) => {
-    return request.body;
+  fastify.addContentTypeParser('application/sdp', { parseAs: "string" }, (req, body, done) => {
+    done(null, body);
   })
 
   fastify.post("/whip", {}, async (request: FastifyRequest, reply: FastifyReply) => {
     try {
-      console.log(request.body);
       // This should of course be modularized
-      /*
       const pionSdpOffer = { type: "offer", sdp: request.body };
+      const pionSdpOfferb64 = Buffer.from(JSON.stringify(pionSdpOffer), "ascii").toString("base64");
       const response = await fetch("http://localhost:8080/sdp", {
         method: "POST",
-        body: JSON.stringify(pionSdpOffer)
+        body: pionSdpOfferb64
       });
-      const data = await response.text();
-      console.log(data);
-      */
+      const pionSdpAnswerb64 = await response.text();
+      console.log(pionSdpAnswerb64);
+      const buf = Buffer.from(pionSdpAnswerb64, "base64");
+      const pionSdpAnswer = JSON.parse(buf.toString("ascii"))
       reply.code(201).headers({
         "content-type": "application/sdp"
-      }).send();
+      }).send(pionSdpAnswer);
     } catch (err) {
       console.error(err);
       reply.code(500).send(err.message);

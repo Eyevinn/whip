206a890199cb0d3739010ae4746af8ca6debbbae
birme <jonas.birme@eyevinn.se>
2022-03-24 17:18:26 +0100

    wip: a bit of cleanup and error handling, and updated readme

--- packages/server/src/wrtc/rtsp.ts
+++ packages/server/src/wrtc/rtsp.ts
@@ -9,13 +9,11 @@ import { fstat, statSync, unlink, unlinkSync } from "fs";
 
 const { RTCVideoSink, RTCAudioSink } = WRTCNonstandard;
 
-var counter = 0;
-
 class UnixStream {
   private locator: string;
 
-  constructor(stream, onSocket) {
-    const socketPath = path.resolve("/tmp/", (++counter).toString() + ".sock");
+  constructor(stream, id, onSocket) {
+    const socketPath = path.resolve("/tmp/", id + ".sock");
     this.locator = "unix:" + socketPath;
 
     try {
@@ -42,8 +40,16 @@ class UnixStream {
   }
 }
 
-function StreamInput(stream) {
-  return new UnixStream(stream, (socket) => socket.pipe(stream));
+function StreamInput(stream, id) {
+  return new UnixStream(stream, id, (socket) => {
+    stream.on("error", (err) => {
+      console.log(`INPUT(${id}):` + err.message);
+    });
+    socket.on("error", (err) => {
+      console.log(`OUTPUT(${id}):` + err.message);
+    })
+    stream.pipe(socket);
+  });
 }
 
 interface WRTCRTSPOptions {
@@ -59,6 +65,73 @@ export class WRTCRTSP extends WHIPResource {
     if (opts && opts.server) {
       this.rtspServer = opts.server;
     }
+  }
+
+  createStream(streamId, audioSink) {
+    const stream = {
+      streamId: streamId,
+      video: new PassThrough(),
+      audio: new PassThrough(),
+      proc: null,
+    };
+
+    const onAudioData = ({ samples: { buffer }}) => {
+      stream.audio.push(Buffer.from(buffer));
+    };
+
+    audioSink.addEventListener("data", onAudioData);
+
+    stream.audio.on("end", () => {
+      audioSink.removeEventListener("data", onAudioData);
+    });
+    stream.audio.on("error", (err) => {
+      console.log("A:" + err.message);
+    });
+
+    stream.video.on("error", (err) => {
+      console.log("V:" + err.message);
+    });
+    
+    stream.proc = ffmpeg()
+      .addInput(StreamInput(stream.video, this.getId() + "-v").url)
+      .addInputOptions([
+        "-f", "rawvideo",
+        "-pix_fmt", "yuv420p",
+        "-s", stream.streamId,
+        "-r", "30",
+      ])
+      .addInput(StreamInput(stream.audio, this.getId() + "-a").url)
+      .addInputOptions([
+        "-f s16le",
+        "-ar 48k",
+        "-ac 2",
+      ])
+      .on("start", (cmdLine) => {
+        console.log("Restreaming started >> " + this.rtspServer + "/" + this.getId());
+        console.log(cmdLine);
+      })
+      .on("codecData", (data) => {
+        console.log(data);
+      })
+      .on("end", () => {
+        console.log("Restreaming stopped >> " + this.rtspServer + "/" + this.getId());
+      })
+      .on("error", (err) => {
+        console.log(`Failed to process video for ${this.getId()}: ` + err.message);
+      })
+      .size(stream.streamId)
+      .output(this.rtspServer + "/" + this.getId())
+      .addOutputOptions([
+        "-rtsp_transport tcp",
+        "-x264-params min-keyint=30",
+        "-g 30"
+      ])
+      .videoCodec("libx264")
+      .audioCodec("aac")
+      .format("rtsp");
+
+    stream.proc.run();
+    return stream;
   }
 
   async beforeAnswer() {
@@ -78,77 +151,15 @@ export class WRTCRTSP extends WHIPResource {
     videoSink.addEventListener("frame", ({ frame: { width, height, data }}) => {
       const streamId = width + "x" + height;
       if (!stream) {
-        stream = {
-          streamId: streamId,
-          video: new PassThrough(),
-          audio: new PassThrough(),
-          end: false,
-          proc: null,
-        };
-
-        const onAudioData = ({ samples: { buffer }}) => {
-          if (!stream.end) {
-            stream.audio.push(Buffer.from(buffer));
-          }
-        };
-        audioSink.addEventListener("data", onAudioData);
-        stream.audio.on("end", () => {
-          audioSink.removeEventListener("data", onAudioData);
-        });
-
-        stream.audio.on("error", (err) => {
-          console.log("A:" + err.message);
-        });
-        stream.video.on("error", (err) => {
-          console.log("V:" + err.message);
-        });
-        
-        stream.proc = ffmpeg()
-          .addInput(StreamInput(stream.video).url)
-          .addInputOptions([
-            "-f", "rawvideo",
-            "-pix_fmt", "yuv420p",
-            "-s", stream.streamId,
-            "-r", "30",
-          ])
-          .addInput(StreamInput(stream.audio).url)
-          .addInputOptions([
-            "-f s16le",
-            "-ar 48k",
-            "-ac 1",
-          ])
-          .on("start", (cmdLine) => {
-            console.log("Restreaming started >> " + this.rtspServer + "/" + this.getId());
-            console.log(cmdLine);
-          })
-          .on("end", () => {
-            console.log("Restreaming stopped >> " + this.rtspServer + "/" + this.getId());
-          })
-          .on("error", (err) => {
-            console.log(`Failed to process video for ${this.getId()}: ` + err.message);
-          })
-          .on("progress", ({ currentFps, frames }) => {
-            console.log(`${this.getId()}: currentFps=${currentFps} (${frames} processed)`);
-          })
-          .size("640x360")
-          .output(this.rtspServer + "/" + this.getId())
-          .addOutputOptions(["-rtsp_transport tcp"])
-          .videoCodec("libx264")
-          .audioCodec("aac")
-          .format("rtsp");
-  
-        stream.proc.run();
+        stream = this.createStream(streamId, audioSink);
       }
       stream.video.push(Buffer.from(data));
     });
 
-    const { close } = this.pc;
+    const { close } = this.pc;
     this.pc.close = () => {
-      console.log("Peer closed, stopping audio and video sink");
-
       audioSink.stop();
       videoSink.stop();
-
       stream.audio.end();
       stream.video.end();
       return close.apply(this, arguments);

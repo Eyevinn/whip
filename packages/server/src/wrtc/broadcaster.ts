afde827a4919d188968e32ef1a48b3473f9ff195
birme <jonas.birme@eyevinn.se>
2022-04-04 11:29:27 +0200

    fix: remove broadcaster channel on disconnect

--- packages/server/src/wrtc/broadcaster.ts
+++ packages/server/src/wrtc/broadcaster.ts
@@ -15,6 +15,12 @@ export class WRTCBroadcaster extends WHIPResource {
     }
   }
 
+  async ondisconnect() {
+    if (this.broadcaster) {
+      this.broadcaster.removeChannel(this.getId());
+    }
+  }
+
   getType() {
     return "broadcaster";
   }

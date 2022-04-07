4823b29d2c8874c9decbf95a9b508efc23d67451
Jonas Birmé <jonas.birme@eyevinn.se>
2022-04-07 09:09:24 +0200

    feat!: ICE gathering timeout (#27)
    * feat: ice candidate gathering timeout can be set
    
    * feat: use eyevinn webrtc player to watch broadcast channel
    
    * chore: use latest version of webrtc-player
    
    * chore: removed init() in favor of a setIceServersFromEndpoint() method
    
    * chore(docs): added comments trying to explain the ICE gathering flow

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,7 @@
 import { parseWHIPIceLinkHeader } from "./util";
 
+const DEFAULT_ICE_GATHERING_TIMEOUT = 2000;
+
 export interface WHIPClientIceServer {
   urls: string;
   username?: string;
@@ -8,9 +10,9 @@ export interface WHIPClientIceServer {
 
 export interface WHIPClientOptions {
   debug?: boolean;
-  iceServers?: WHIPClientIceServer[],
+  iceServers?: WHIPClientIceServer[];
+  iceGatheringTimeout?: number;
   authkey?: string;
-  iceConfigFromEndpoint?: boolean,
 }
 
 export interface WHIPClientConstructor {
@@ -25,6 +27,9 @@ export class WHIPClient {
   private peer: RTCPeerConnection;
   private resource: string;
   private resourceResolve: (resource: string) => void;
+  private iceGatheringTimeout;
+  private iceGatheringComplete: boolean;
+  private onIceCandidateFn: ({ candidate: RTCIceCandidate }) => void;
 
   constructor({ endpoint, opts }: WHIPClientConstructor) {
     this.whipEndpoint = new URL(endpoint);
@@ -41,8 +46,8 @@ export class WHIPClient {
     this.peer.onicegatheringstatechange = this.onIceGatheringStateChange.bind(this);
     this.peer.oniceconnectionstatechange =
       this.onIceConnectionStateChange.bind(this);
-    this.peer.onicecandidate = this.onIceCandidate.bind(this);
     this.peer.onicecandidateerror = this.onIceCandidateError.bind(this);
+    this.onIceCandidateFn = this.onIceCandidate.bind(this);
   }
 
   private log(...args: any[]) {
@@ -65,31 +70,12 @@ export class WHIPClient {
 
   private async onIceCandidate({ candidate }) {
     if (candidate === null) {
-      const response = await fetch(this.whipEndpoint.toString(), {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/sdp",
-          "Authorization": this.opts.authkey
-        },
-        body: this.peer.localDescription.sdp,
-      });
+      // ICE gathering is complete we clear the timeout
+      // and send the updated SDP to the server peer.
+      clearTimeout(this.iceGatheringTimeout);
 
-      if (response.ok) {
-        this.resource = response.headers.get("Location");
-        this.log("WHIP Resource", this.resource);
-        if (this.resourceResolve) {
-          this.resourceResolve(this.resource);
-          this.resourceResolve = null;
-        }
-
-        const answer = await response.text();
-        this.peer.setRemoteDescription({
-          type: "answer",
-          sdp: answer,
-        });
-      } else {
-        this.error("IceCandidate", "Failed to setup stream connection with endpoint", response.status, await response.text());
-      }
+      this.peer.removeEventListener("icecandidate", this.onIceCandidateFn);
+      this.onIceGatheringComplete();
     } else {
       this.log("IceCandidate", candidate.candidate);
     }
@@ -99,41 +85,79 @@ export class WHIPClient {
     this.log("IceCandidateError", e);
   }
 
-  async ingest(mediaStream: MediaStream): Promise<void> {
-    if (this.opts.iceConfigFromEndpoint) {
-      const iceServers: WHIPClientIceServer[] = await this.doFetchICEFromEndpoint();
-      this.peer.setConfiguration({ iceServers: iceServers });
+  private onIceGatheringTimeout() {
+    this.log("IceGatheringTimeout");
+    clearTimeout(this.iceGatheringTimeout);
+
+    this.peer.removeEventListener("icecandidate", this.onIceCandidateFn);
+    this.onIceGatheringComplete();
+  }
+
+  private async onIceGatheringComplete() {
+    if (this.iceGatheringComplete) {
+      return;
     }
+    
+    // We are ready to send an updated SDP to server peer
+    this.log("IceGatheringComplete");
 
-    mediaStream
-      .getTracks()
-      .forEach((track) => this.peer.addTrack(track, mediaStream));
+    this.iceGatheringComplete = true;
 
+    const response = await fetch(this.whipEndpoint.toString(), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/sdp",
+        "Authorization": this.opts.authkey
+      },
+      body: this.peer.localDescription.sdp,
+    });
+
+    if (response.ok) {
+      this.resource = response.headers.get("Location");
+      this.log("WHIP Resource", this.resource);
+      if (this.resourceResolve) {
+        this.resourceResolve(this.resource);
+        this.resourceResolve = null;
+      }
+
+      const answer = await response.text();
+      this.peer.setRemoteDescription({
+        type: "answer",
+        sdp: answer,
+      });
+    } else {
+      this.error("IceCandidate", "Failed to setup stream connection with endpoint", response.status, await response.text());
+    }
+  }
+
+  private async startSdpExchange(): Promise<void> {
+    // https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Connectivity
+    // 
+    // Client peer creates an offer
     const sdpOffer = await this.peer.createOffer({
       offerToReceiveAudio: false,
       offerToReceiveVideo: false,
     });
+    // We store the offer as the local SDP. This will trigger the ICE candidate gathering
+    // process. The client will ask the STUN/TURN servers (iceServers) for a set of candidates
     this.peer.setLocalDescription(sdpOffer);
-  }
 
-  async destroy(): Promise<void> {
-    const resourceUrl = await this.getResourceUrl();
-    await fetch(resourceUrl, { method: "DELETE" }).catch((e) => this.error("destroy()", e));
-  }
+    // When we get an ICE candidate we get an 'icecandidate' event with
+    // an RTCIceCandidate. When this candidate is null we know we have
+    // gathered all candidates
+    this.peer.addEventListener("icecandidate", this.onIceCandidateFn);
 
-  getResourceUrl(): Promise<string> {
-    if (this.resource) {
-      return Promise.resolve(this.resource);
-    }
-    return new Promise((resolve) => {
-      // resolved in onIceCandidate`
-      this.resourceResolve = resolve;
-    });
+    // As part of the ICE gathering process the client will test connection for each candidate
+    // which can take some time if some of the candidates are slow to timeout. We need to
+    // set a timeout where we will send what we have. We might have all candidates but the
+    // 'null' candidate does not arrive until we all candidate checks are completed.
+    this.iceGatheringComplete = false;
+    this.iceGatheringTimeout = setTimeout(this.onIceGatheringTimeout.bind(this), this.opts.iceGatheringTimeout || DEFAULT_ICE_GATHERING_TIMEOUT);
   }
 
   private async doFetchICEFromEndpoint(): Promise<WHIPClientIceServer[]> {
     let iceServers: WHIPClientIceServer[] = [];
-    const response = await fetch(this.whipEndpoint.href, {
+    const response = await fetch(this.whipEndpoint.toString(), {
       method: "OPTIONS",
       headers: {
         "Authorization": this.opts.authkey,
@@ -150,5 +174,38 @@ export class WHIPClient {
       });
     }
     return iceServers;
+  }  
+
+  async setIceServersFromEndpoint(): Promise<void> {
+    if (this.opts.authkey) {
+      this.log("Fetching ICE config from endpoint");
+      const iceServers: WHIPClientIceServer[] = await this.doFetchICEFromEndpoint();
+      this.peer.setConfiguration({ iceServers: iceServers });
+    } else {
+      this.error("No authkey is provided so cannot fetch ICE config from endpoint.");
+    }
+  }
+
+  async ingest(mediaStream: MediaStream): Promise<void> {
+    mediaStream
+      .getTracks()
+      .forEach((track) => this.peer.addTrack(track, mediaStream));
+      
+    await this.startSdpExchange();
+  }
+
+  async destroy(): Promise<void> {
+    const resourceUrl = await this.getResourceUrl();
+    await fetch(resourceUrl, { method: "DELETE" }).catch((e) => this.error("destroy()", e));
+  }
+
+  getResourceUrl(): Promise<string> {
+    if (this.resource) {
+      return Promise.resolve(this.resource);
+    }
+    return new Promise((resolve) => {
+      // resolved in onIceCandidate`
+      this.resourceResolve = resolve;
+    });
   }
 }

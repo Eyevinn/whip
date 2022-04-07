4ab6d33e4aacca019576c17be6113c171fc7e543
birme <jonas.birme@eyevinn.se>
2022-04-07 22:02:47 +0200

    feat: show viewer count in watch window

--- packages/demo/src/watch.ts
+++ packages/demo/src/watch.ts
@@ -1,10 +1,15 @@
-import { watch } from "./util";
+import { getViewerCount, watch } from "./util";
 
 window.addEventListener("DOMContentLoaded", async () => {
   const searchParams = new URL(window.location.href).searchParams;
   const locator = searchParams.get("locator");
 
   if (locator) {
-    await watch(locator, document.querySelector<HTMLVideoElement>("video"))
+    await watch(locator, document.querySelector<HTMLVideoElement>("video"));
+
+    const t = setInterval(async () => { 
+      const viewerCount = await getViewerCount(locator);
+      document.querySelector<HTMLDivElement>("#viewercount").innerHTML = `${viewerCount} viewers`;  
+    }, 5000);
   }
 }); 

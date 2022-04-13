579123dc5800055bdf4957f4fcad6f01e8d6116b
birme <jonas.birme@eyevinn.se>
2022-04-13 21:27:16 +0200

    fix: possible to set ext port with env variable

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -34,7 +34,7 @@ if (process.env.TLS_TERMINATION_ENABLED) {
 
 const broadcaster = new Broadcaster({ 
   port: parseInt(process.env.BROADCAST_PORT || "8001"),
-  extPort: parseInt(process.env.BROADCAST_PORT || "8001"),
+  extPort: parseInt(process.env.BROADCAST_EXT_PORT || "8001"),
   hostname: process.env.BROADCAST_HOSTNAME,
   https: process.env.BROADCAST_USE_HTTPS && process.env.BROADCAST_USE_HTTPS === "true",
   tls: tlsOptions,
@@ -45,7 +45,7 @@ broadcaster.listen();
 
 const endpoint = new WHIPEndpoint({ 
   port: parseInt(process.env.PORT || "8000"), 
-  extPort: parseInt(process.env.PORT || "8000"),
+  extPort: parseInt(process.env.EXT_PORT || "8000"),
   hostname: process.env.WHIP_ENDPOINT_HOSTNAME,
   https: process.env.WHIP_ENDPOINT_USE_HTTPS && process.env.WHIP_ENDPOINT_USE_HTTPS === "true",
   tls: tlsOptions,
